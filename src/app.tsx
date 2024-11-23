555a08119d8d8151922f47594e3b1ae9ecde389d
league-of-handsome-gentlemen <league-of-handsome-gentlemen@users.noreply.github.com>
2024-11-23 12:04:46 -0500

    Set router base path dynamically

--- src/app.tsx
+++ src/app.tsx
@@ -3,18 +3,21 @@ import { LoginPage } from './login/login-page';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { PrivateRoutes } from './private-routes';
 
-export const App = () => (
-  <div className="container">
-    <BrowserRouter basename='/portal'>
-      <Routes>
-        <Route element={<PrivateRoutes />}>
-          <Route path='/' element={<h1>Home</h1>}></Route>
-          <Route path='/other' element={<h1>Other</h1>}></Route>
-        </Route>
-        <Route path='/login' element={<LoginPage />}></Route>
-        
-      </Routes>
-    </BrowserRouter>
+export const App = () => {
+  const { NODE_ENV } = process.env;
+  const baseName = NODE_ENV === 'production' ? '/portal' : '/';
 
-  </div>
-);
+  return (
+    <div className="container">
+      <BrowserRouter basename={baseName}>
+        <Routes>
+          <Route element={<PrivateRoutes />}>
+            <Route path='/' element={<h1>Home</h1>}></Route>
+            <Route path='/other' element={<h1>Other</h1>}></Route>
+          </Route>
+          <Route path='/login' element={<LoginPage />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+};
